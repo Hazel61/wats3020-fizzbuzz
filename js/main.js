64fb288832119eb96e389f40593d4f814caec35d
Shannon Michaels <michaelsshan@seattleu.edu>
2019-01-27 16:23:26 -0800

    Third commit. Program works as designed. Added comments and deleted todo's.

--- js/main.js
+++ js/main.js
@@ -1,21 +1,14 @@
 /* Code for WATS 3020 FizzBuzz Assignment */
+/* Create a program that takes a maximum number and makes sure it is an integer greater than zero. Design logic that finds if the numbers between 1 and the maximum number are divisible by 3, 5, and 15. If divisible by 3, store Fizz, if divisible by 5, store Buzz, and if both, store FizzBuzz.
+ */
 
 let isInteger,
     maxNumber,
     fbResults,
     fbText;
 
-
-// TODO: Initialize a variable `isInteger` to use as a control value for the
-// `while` loop. Set the initial value to `false`.
-
 isInteger = false;
-
-// TODO: Create a `while` loop so user will be prompted to enter a number until
-// they enter a good number. This loop should be controlled by a conditional
-// expression that looks at the value of `isSafeInteger`. When `isSafeInteger`
-// becomes `true`, the `while` loop should stop looping.
-
+// take input
 while (isInteger = !isInteger) {
     let input = window.prompt ("Please enter a number 1 or greater. ");
     if (input === null) break;
@@ -24,56 +17,30 @@ while (isInteger = !isInteger) {
         isInteger = Number.isSafeInteger(maxNumber) && maxNumber > 0;
     }
 }
+// find results
 fbResults = [];
 for (let i = 1; i <= maxNumber; i++) {
 
    let tempString = "";
    if (i % 3 === 0) {
-       tempString = "Fizz";
+       tempString += "Fizz";4
    }
    if (i % 5 === 0){
-       tempString = "Buzz";
-   }
-   if (i % 5 === 0 && i % 3 === 0) {
-       tempString = "FizzBuzz";
+       tempString += "Buzz";
    }
 
-   if (tempString = "") {
+   if (tempString === "") {
        fbResults.push(i);
    }   else {
        fbResults.push(tempString);
    }
 }
-
+// combine results for output
 fbText = "";
-
 for (i = 0; i < fbResults.length; i++){
     fbText = fbText + fbResults[i] + "\n";
 }
-console.log (fbText);
-// TODO: Inside `while` loop prompt the user for the `maxNumber` value.
-
-// TODO: Inside the `while` loop, use a conditional to verify if the `maxNumber`
-// is a suitable integer. (It should be an integer greater than zero.)
-
-// TODO: If the value of `maxNumber` is suitable, then change the value of
-// `isInteger` so the `while` loop stops looping.
-
-// TODO: Initialize the `fbResults` variable to an empty Array
-
-// TODO: Create a `for` loop that will execute the `maxNumber` of times.
-
-// TODO: Create logic inside the `for` loop to calculate FizzBuzz.  This will
-// require the use of several conditional statements that use the `%` operator.
-// Store the results of this logic in an array called `fbResults`.
-
-// TODO: In preparation for creating the output text for your FizzBuzz results,
-// initialize the `fbText` variable to an empty string.
-
-// TODO: Use a `for ... of` loop to concatenate the values from `fbResults`
-// into the variable `fbText`.
-
-
+// display results
 ///////////////////////////////////////////////////////////////////////
 // Do not edit below this line unless you are doing something fancy!
 //////////////////////////////////////////////////////////////////////
